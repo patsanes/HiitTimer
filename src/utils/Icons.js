878ccf6331a5152067be04d117476c87e5f28439
Patricia Sanes <psanesl93@gmail.com>
2019-07-07 17:01:19 -0300

    Icons refactor

--- src/utils/Icons.js
+++ src/utils/Icons.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import IconI from 'react-native-vector-icons/Ionicons';
-import IconA from 'react-native-vector-icons/AntDesign';
-import IconE from 'react-native-vector-icons/Entypo';
+import IconFontAwesome from 'react-native-vector-icons/FontAwesome';
+import IconIonicons from 'react-native-vector-icons/Ionicons';
+import IconAntDesign from 'react-native-vector-icons/AntDesign';
+import IconEntypo from 'react-native-vector-icons/Entypo';
 import PropTypes from 'prop-types';
 
 const styles = StyleSheet.create({
@@ -16,31 +16,21 @@ const styles = StyleSheet.create({
     borderColor: 'white',
   },
 });
-export default class Icons extends React.Component {
-  nested_Icons = name => {
-    if (name == 'Countdown') {
-      return <IconI name="ios-fitness" size={16} color="white" style={styles.iconList} />;
-    }
-    if (name == 'Rest') {
-      return <IconA name="rest" size={16} color="white" style={styles.iconList} />;
-    }
-    if (name == 'Cycle') {
-      return <IconE name="cycle" size={16} color="white" style={styles.iconList} />;
-    }
-    if (name == 'Serie') {
-      return <Icon name="spinner" size={16} color="white" style={styles.iconList} />;
-    }
-    if (name == 'Rest between cycles') {
-      return <IconI name="ios-timer" size={16} color="white" style={styles.iconList} />;
-    }
-    return <IconE name="stopwatch" size={16} color="white" style={styles.iconList} />;
-  };
-  render() {
-    const { name } = this.props;
-    return this.nested_Icons(name);
-  }
-}
+const iconsSettings = {
+  Countdown: { name: 'ios-fitness', Icon: IconIonicons },
+  Rest: { name: 'rest', Icon: IconAntDesign },
+  Cycle: { name: 'cycle', Icon: IconEntypo },
+  Serie: { name: 'spinner', Icon: IconFontAwesome },
+  'Rest between cycles': { name: 'ios-timer', Icon: IconIonicons },
+  'Start countdown': { name: 'stopwatch', Icon: IconEntypo },
+};
+const Icons = ({ name: iconName }) => {
+  const { name, Icon } = iconsSettings[iconName];
+  return <Icon name={name} size={16} color="white" style={styles.iconList} />;
+};
 
 Icons.propTypes = {
   name: PropTypes.string.isRequired,
 };
+
+export default Icons;

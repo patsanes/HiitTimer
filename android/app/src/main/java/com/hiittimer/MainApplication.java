db39806c790ee23f04fee59572cf77436e532f60
Patricia Sanes <psanesl93@gmail.com>
2019-08-08 20:38:33 -0300

    fix build

--- android/app/src/main/java/com/hiittimer/MainApplication.java
+++ android/app/src/main/java/com/hiittimer/MainApplication.java
@@ -7,6 +7,7 @@ import com.facebook.react.PackageList;
 import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
 import com.facebook.react.bridge.JavaScriptExecutorFactory;
 import com.facebook.react.ReactApplication;
+import com.swmansion.reanimated.ReanimatedPackage;
 import io.invertase.firebase.RNFirebasePackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
---
+++ ios/File.swift
@@ -0,0 +1,11 @@
+//
+//  File.swift
+//  HiitTimer
+//
+//  Created by Patricia Sanes on 8/8/19.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+// https://stackoverflow.com/a/54586937
+// https://stackoverflow.com/a/55419920
+import Foundation
